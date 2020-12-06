9244df4668948a6f852f8bc54d9fc5be8591a42b
Osman Aşar <osmanasar933@gmail.com>
2020-12-06 23:33:56 +0300

    başlangıç

---
+++ index.js
@@ -0,0 +1,51 @@
+const { app, BrowserWindow , ipcMain , Notification} = require('electron')
+const path = require("path")
+const url = require("url")
+const process = require("child_process");
+app.allowRendererProcessReuse = false
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
+
+app.on('ready',() => {
+    process.execSync("find . -name '.DS_Store' -type f -delete");
+    ipcMain.on('fail',(err,data) => {
+        const notification = {
+          title: 'Basic Notification',
+          body: 'Kişi Bulunamadı'
+      }
+      new Notification(notification).show()
+    })
+
+    ipcMain.on('ok',(err,data) => {
+        const notification = {
+          title: 'Basic Notification',
+          body: data + ' işleminiz alınmıştır'
+      }
+      new Notification(notification).show()
+    })
+    
+})
+
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
